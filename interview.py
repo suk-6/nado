614199ba4067f9369759d7a50d0f0fd11b725897
suk-6 <me@suk.kr>
2024-04-30 08:45:50 +0900

    Add Interview class with save and get methods

--- interview.py
+++ interview.py
@@ -1,3 +1,15 @@
 class Interview:
     def __init__(self) -> None:
         pass
+
+    def saveInterviewQuestion(self, question):
+        pass
+
+    def getInterviewQuestion(self, questionID=None):
+        pass
+
+    def saveInterviewAnswer(self, questionID, answer):
+        pass
+
+    def getInterviewAnswer(self, questionID):
+        pass
